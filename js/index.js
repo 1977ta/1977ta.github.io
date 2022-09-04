357de844ab1c9e654cd3bbac97a0a543a2e2069b
1977ta <111229437+1977ta@users.noreply.github.com>
2022-09-05 04:50:57 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,19 +1,38 @@
 'use strict';
+// header
+{
+  const open=document.getElementById('open');
+  const overlay=document.querySelector('.overlay');
+  const close=document.getElementById('close');
+
+  open.addEventListener('click',()=>{
+    overlay.classList.add('show')
+    open.classList.add('hide')
+  })
+
+  close.addEventListener('click',()=>{
+    overlay.classList.remove('show')
+    open.classList.remove('hide')
+  })
+}
+
+// hero
 {
   function play(){
     setTimeout(()=>{
       images[currentIndex].classList.remove('current');
       currentIndex++;
-      if(currentIndex>images.length-1){
-        currentIndex=0;
+       if(currentIndex>images.length-1){
+      currentIndex=0;
       }
       images[currentIndex].classList.add('current');
       play();
     },5000);
   }
 
-  const images=document.querySelectorAll('.hero img');
-  let currentIndex=0;
+    const images=document.querySelectorAll('.hero img');
+    let currentIndex=0;
 
-  play();
+    play();
+
 }
